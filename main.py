c29f16c2149bd827892071fea49a70810c44d9ee
jessica <jebianca10@gmail.com>
2023-10-07 18:30:12 -0300

    segunda versão

--- main.py
+++ main.py
@@ -5,17 +5,17 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-import win32com.client as win32
 from time import sleep
 import openpyxl
+import smtplib
 import re
 
 class Scrappy:
     def iniciar(self):
         self.email_usuario()
-        self.login_site()
-        self.raspagem_de_dados()
-        self.colocar_dados_excel()
+        #self.login_site()
+        #self.raspagem_de_dados()
+        #self.colocar_dados_excel()
         self.enviar_email_usuario()
     def email_usuario(self):
         self.email = input('Digite o email para receber o relatorio de média:\n')
@@ -99,23 +99,31 @@ class Scrappy:
         print('Planilha atualizada com sucesso!')
 
     def enviar_email_usuario(self):
-        outlook = win32.Dispatch('outlook.application')
-        mail = outlook.CreateItem(0)
-        anexo = fr'C:\Users\jessica\PycharmProjects\AutomacaoExcelNotas\Fiquei_na_media-Caires.xlsx'
-        mail.To = self.email
-        mail.Attachments.Add(anexo)
-        mail.Subject = 'Excel para Saber se Ficou na Média'
-        mail.HTMLBody = f'''
-        <p>Prezado,</p>
+        endereco = input('Digite seu email aqui: ')
+        senha = input('Digite sua senha aqui: ')
+        arquivo = 'Fiquei_na_media-Caires.xlsx'
 
-        <p>Segue o Excel para saber se ficou na média.</p>
+        try:
+            servidor_email = smtplib.SMTP('smtp.gmail.com', 587)
+            servidor_email.starttls()
+            servidor_email.login(endereco, senha)
 
-        <p>att.,</p>
-        <p>Jesbica</p>
-        '''
+            remetente = 'seu_email@gmail.com'
+            destinatarios = self.email
+            assunto = 'Excel para Saber se Ficou na Média'
+            conteudo = ('<p>Segue o Excel para saber se ficou na média.</p>'
+                        '<p>att.,</p>'
+                        '<p>Jesbica</p>')
 
-        mail.Send()
-        print('email enviado')
+
+            message = (f"From:{remetente}\nSubject:{assunto}\n\n{conteudo}\n"
+                       f"{arquivo}, maintype='application',subtype='octet-stream', filename='Fiquei_na_media-Caires.xlsx'")
+
+            servidor_email.sendmail(remetente, destinatarios, message)
+        except Exception as e:
+            print(f"Erro ao enviar email: {e}")
+        finally:
+            servidor_email.quit()
 
 start = Scrappy()
 start.iniciar()
